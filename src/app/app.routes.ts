ad1b812cecaa0f7de4b75e38c8bb6482b8cdb7d1
edupoly <info.edupoly@gmail.com>
2025-11-13 14:07:18 +0530

    ngrx-basics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Counter } from './counter/counter';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:Counter
+    }
+];
